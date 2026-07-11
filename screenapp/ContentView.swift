86a5e6391662ee0a8906955ac6cd4ab47591848f
Ashmi <ashmi.sharma02@gmail.com>
2026-07-11 10:01:34 +0800

    added phaseanimator

--- screenapp/ContentView.swift
+++ screenapp/ContentView.swift
@@ -10,14 +10,20 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-		VStack {
-			Text("Daniel")
-				.font(.title.weight(.medium).width(.expanded))
-				.foregroundStyle(.white)
-            Text("Ashmi")
-                .font(.title2.weight(.medium).width(.expanded))
-                .foregroundStyle(.cyan)
-		}
+        VStack(spacing: 50) {
+            PhaseAnimator([0,1,3]){ value in
+                Text("Daniel")
+                    .font(.title.weight(.medium).width(.expanded))
+                    .foregroundStyle(.white)
+                    .scaleEffect(value)
+                    .opacity(value == 1 ? 1 : 0)
+                Text("Ashmi")
+                    .font(.title2.weight(.medium).width(.expanded))
+                    .foregroundStyle(.cyan)
+                    .scaleEffect(3 - value)
+                    .opacity(value == 1 ? 1 : 0)
+            }
+        }
 		.frame(maxWidth: .infinity, maxHeight: .infinity)
 		.ignoresSafeArea()
 		.background(.black)
