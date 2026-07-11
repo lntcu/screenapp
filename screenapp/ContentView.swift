342092d8a8a3194f8533ba8a6a82caa18d43ff94
Ashmi <ashmi.sharma02@gmail.com>
2026-07-11 09:55:36 +0800

    added my name underneath

--- screenapp/ContentView.swift
+++ screenapp/ContentView.swift
@@ -11,9 +11,12 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
 		VStack {
-			Text("Ashmi")
+			Text("Daniel")
 				.font(.title.weight(.medium).width(.expanded))
 				.foregroundStyle(.white)
+            Text("Ashmi")
+                .font(.title2.weight(.medium).width(.expanded))
+                .foregroundStyle(.cyan)
 		}
 		.frame(maxWidth: .infinity, maxHeight: .infinity)
 		.ignoresSafeArea()
